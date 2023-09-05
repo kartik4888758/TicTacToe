467fd608ebe008878d298e6fcab8c09ab9614a81
Kartik_kr <kartik.kumar@adypu.edu.in>
2023-09-05 19:29:54 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+// Function to print the Tic-Tac-Toe board
+void printBoard(char board[3][3]) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        cout << endl;
+        if (i < 2) cout << "---------" << endl;
+    }
+}
+
+// Function to check if a player has won the game
+bool checkWin(char board[3][3], char player) {
+    // Check rows and columns
+    for (int i = 0; i < 3; ++i) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+            return true;
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
+            return true;
+    }
+
+    // Check diagonals
+    if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+        return true;
+    if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+        return true;
+
+    return false;
+}
+
+int main() {
+    char board[3][3] = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+    char currentPlayer = 'X';
+
+    cout << "Welcome to Tic-Tac-Toe!" << endl;
+
+    while (true) {
+        cout << "Current board:" << endl;
+        printBoard(board);
+        cout << "Player " << currentPlayer << ", enter row and column (e.g., 1 2): ";
+
+        int row, col;
+        cin >> row >> col;
+
+        // Check if the input is valid
+        if (row < 1 || row > 3 || col < 1 || col > 3 || board[row - 1][col - 1] != ' ') {
+            cout << "Invalid move. Try again." << endl;
+            continue;
+        }
+
+        board[row - 1][col - 1] = currentPlayer;
+
+        if (checkWin(board, currentPlayer)) {
+            cout << "Player " << currentPlayer << " wins!" << endl;
+            break;
+        }
+
+        // Switch to the other player
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    cout << "Thanks for playing!" << endl;
+
+    return 0;
+}
